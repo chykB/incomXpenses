d1fb5ca8ae4a600c2d5e5a453f294e56a9d5b8d6
chykB <malikchika86@gmail.com>
2025-03-29 21:03:45 +0100

    Feat: Added search functionality

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('expenses', views.add_expences, name="expenses"),
+    path('edit_expense/<int:id>', views.edit_expenses, name="edit_expense"),
+    path('delete_expense/<int:id>', views.delete_expense, name="delete_expense"),
+    path('search_expenses', csrf_exempt(views.search_expenses), name="search_expenses")
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -2,12 +2,36 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Expense, Category
 from django.contrib import messages
+from django.core.paginator import Paginator
+import json
+from django.http import JsonResponse
+
 
 # Create your views here.
+def search_expenses(request):
+    if request.method == "POST":
+        search_str = json.loads(request.body).get("searchText")
+        expenses = Expense.objects.filter(
+            amount__startswith=search_str, owner=request.user) | Expense.objects.filter(
+            date__startswith=search_str, owner=request.user) | Expense.objects.filter(
+            description__icontains=search_str, owner=request.user) | Expense.objects.filter(
+            category__icontains=search_str, owner=request.user)
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
+
+
 @login_required(login_url="/authentication/login")
 def index(request):
     categories = Category.objects.all()
-    return render(request, "expenses/index.html")
+    expenses = Expense.objects.filter(owner=request.user)
+    paginator = Paginator(expenses, 3)
+    page_number = request.GET.get("page")
+    page_object = Paginator.get_page(paginator, page_number)
+    context = {
+        "expenses":expenses,
+        "page_object":page_object
+    }
+    return render(request, "expenses/index.html", context)
 
 def add_expences(request):
     categories = Category.objects.all()
@@ -36,4 +60,50 @@ def add_expences(request):
     Expense.objects.create(owner=request.user, amount=amount, description=description, 
                            date=date, category=category)
     messages.success(request, "Expense saved successfully")
+    return redirect("index")
+
+
+def edit_expenses(request, id):
+    expense = Expense.objects.get(pk=id)
+    categories = Category.objects.all()
+    context = {
+        "expense": expense,
+        "values": expense,
+        "categories" :categories,
+    }
+    if request.method == "GET":
+        return render(request, "expenses/edit-expense.html", context)
+    if request.method == "POST":
+        amount = request.POST["amount"]
+        if not amount:
+            messages.error(request, "Amount is required")
+            return render(request, "expenses/edit-expense.html", context)
+        date = request.POST["expense_date"]
+        if not date:
+            messages.error(request, "Date is required")
+            return render(request, "expenses/edit-expense.html", context)
+   
+        description = request.POST["description"]
+        
+        category = request.POST["category"]
+        if not description:
+            messages.error(request, "Description is required")
+            return render(request, "expenses/edit-expense.html", context)
+        
+        expense.owner = request.user
+        expense.amount = amount
+        expense.description=description
+        expense.date=date
+        expense.category=category
+        expense.save()
+
+        messages.success(request, "Expense updated successfully")
+        return redirect("index")
+        # messages.info(request, "Handling post form")
+        # return render(request, "expenses/edit-expense.html", context)
+
+def delete_expense(request, id):
+    expense = Expense.objects.get(pk=id)
+    expense.delete()
+    messages.success(request, "Expense deleted successfully")
     return redirect("index")
